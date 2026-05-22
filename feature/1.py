4436f725171b9732e7011e0d43f7b6bd5cfd8a2c
dhruvd-kombe <dhruv.d.kombee@outlook.com>
2026-05-22 10:47:43 +0530

    delect unwanted files

--- feature/1.py
+++
@@ -1,2 +0,0 @@
-print("this is for the login branch " )
-print("learning of the working structure of the branch and pr requestes ")
--- fpy/f.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-print("f")
-a = 2
-b = 2
-c = 2*2
-print("value of the C = ", c)
-print(a)
-print(a)
