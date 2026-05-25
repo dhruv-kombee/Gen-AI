d98f70c678b51fd54bcf34cf3ca79da15a4679f1
dhruvd-kombe <dhruv.d.kombee@outlook.com>
2026-05-25 12:00:33 +0530

    Environment and development setting basics learned

---
+++ 25-May/Development/app.py
@@ -0,0 +1,4 @@
+print("Made file by , new-item app.py")
+print("Rename form the api.py to app.py by,  mv f1 f2")
+print("Made file by , new-item app.py")
+print("Copy form the api.py to app.py by , copy f1 f2")
